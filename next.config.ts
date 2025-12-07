e0185c88784002f6602e0394eaf8f4d1eca21b4d
Pranavtej1404 <npranavtej@gmail.com>
2025-12-07 20:17:57 +0530

    removed two files

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  productionBrowserSourceMaps: false,
-  devIndicators: { buildActivity: false },
+  
 };
 
 export default nextConfig;
